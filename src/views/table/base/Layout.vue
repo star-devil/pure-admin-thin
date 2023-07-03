0f624df1647bf0398ce209dcb23829f962fef497
wangQiaoling <wangqiaoling1997@gmail.com>
2023-07-03 15:03:29 +0800

    feat: 表格布局

---
+++ src/views/table/base/Layout.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Author: wangqiaoling
+ * @Date: 2023-07-03 14:08:08
+ * @LastEditTime: 2023-07-03 14:59:18
+ * @LastEditors: wangqiaoling
+ * @Description: 表格布局
+-->
+<script setup lang="ts">
+import { ref } from "vue";
+import { tableData } from "./data";
+
+type TableLayout = "fixed" | "auto";
+
+const tableLayout = ref<TableLayout>("fixed");
+
+const columns: TableColumnList = [
+  {
+    label: "日期",
+    prop: "date"
+  },
+  {
+    label: "姓名",
+    prop: "name"
+  },
+  {
+    label: "地址",
+    prop: "address"
+  }
+];
+</script>
+
+<template>
+  <div>
+    <el-link
+      href="https://developer.mozilla.org/en-US/docs/Web/CSS/table-layout"
+      target="_blank"
+      style="margin: 0 4px; font-size: 14px"
+    >
+      #table-layout
+    </el-link>
+    <el-radio-group v-model="tableLayout">
+      <el-radio-button label="fixed" />
+      <el-radio-button label="auto" />
+    </el-radio-group>
+
+    <pure-table
+      :data="tableData"
+      :columns="columns"
+      :table-layout="tableLayout"
+    />
+  </div>
+</template>
--- src/views/table/base/list.tsx
+++ src/views/table/base/list.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: wangqiaoling
  * @Date: 2023-04-04 13:43:05
- * @LastEditTime: 2023-07-03 13:46:07
+ * @LastEditTime: 2023-07-03 14:49:09
  * @LastEditors: wangqiaoling
  * @Description: 渲染表格的数据list
  */
@@ -24,6 +24,7 @@ import Tree from "./Tree.vue";
 import TableRow from "./TotalRow.vue";
 import Merge from "./Merge.vue";
 import CustomIndex from "./CustomIndex.vue";
+import Layout from "./Layout.vue";
 
 const rendContent = (val: string) =>
   `代码位置：src/views/table/base/${val}.vue`;
@@ -148,5 +149,11 @@ export const list = [
     content: rendContent("CustomIndex"),
     title: "自定义索引",
     component: CustomIndex
+  },
+  {
+    key: "layout",
+    content: rendContent("Layout"),
+    title: "表格布局",
+    component: Layout
   }
 ];
